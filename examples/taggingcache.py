fd225ea8553d13b291598dc4b3de38db6197d11e
Edward George <edwardgeorge@gmail.com>
2010-03-14 22:20:41 +0000

    added some demo code with tests. an implementation of memcache-tag.

---
+++ examples/taggingcache.py
@@ -0,0 +1,62 @@
+"""Tagging store example for shoveserver.
+
+implements the commands added in http://code.google.com/p/memcached-tag/
+
+ - tag_add <tag> <key>
+ - tag_delete <tag>
+
+"""
+import shoveserver
+from shoveserver import exceptions
+
+TAG_STORED = 'TAG_STORED'
+TAG_DELETED = 'TAG_DELETED'
+TAG_NOT_FOUND = 'TAG_NOT_FOUND'
+
+
+class TagNotFoundError(exceptions.MemcacheProtocolException):
+    response = TAG_NOT_FOUND
+
+
+class TaggingStore(shoveserver.stores.Store):
+    def __init__(self, *args, **kwargs):
+        super(TaggingStore, self).__init__(*args, **kwargs)
+        self.tags = {}
+        self.key_tags = {}
+
+    def tag_add(self, file, tag, key):
+        if not key in self.store:
+            raise TagNotFoundError(key)
+        self.tags.setdefault(tag, set()).add(key)
+        self.key_tags.setdefault(key, set()).add(tag)
+
+    def tag_delete(self, file, tag):
+        try:
+            tagged = self.tags.pop(tag)
+        except KeyError, e:
+            raise TagNotFoundError(tag)
+        else:
+            for key in tagged:
+                del self.store[key]
+                self.key_tags[key].remove(tag)
+
+
+def taggingserver(store):
+    server = shoveserver.server.MemcacheServer(store)
+    server.add_command(['tag_add'],
+        r'^(?P<tag>\S{1,250})\s(?P<key>\S{1,250})$', TAG_STORED)
+    server.add_command(['tag_delete'],
+        r'^(?P<tag>\S{1,250})$', TAG_DELETED)
+    return server
+
+
+def serve(d, host='127.0.0.1', port=11211):
+    store = TaggingStore(d)
+    sock = eventlet.listen((host, port))
+    server = taggingserver(store)
+    shoveserver.server.serve_store(sock, store, server=server)
+
+
+if __name__ == '__main__':
+    serve({})
+
---
+++ tests/example_tagging_test.py
@@ -0,0 +1,81 @@
+import functools
+
+from examples import taggingcache
+from tests import server_request
+
+
+def test_tagging_key():
+    data = {}
+    store = taggingcache.TaggingStore(data, writeable=True)
+    server = taggingcache.taggingserver(store)
+    resp = server_request(server, 'set testkey 0 0 4\r\n'
+                                  'blah\r\n')
+    assert resp == 'STORED\r\n'
+
+    resp = server_request(server, 'tag_add testtag testkey\r\n')
+    assert resp == 'TAG_STORED\r\n'
+
+
+def test_deleting_tag():
+    data = {}
+    store = taggingcache.TaggingStore(data, writeable=True)
+    server = taggingcache.taggingserver(store)
+    resp = server_request(server, 'set testkey 0 0 4\r\n'
+                                  'blah\r\n')
+    assert resp == 'STORED\r\n'
+
+    resp = server_request(server, 'tag_add testtag testkey\r\n')
+    assert resp == 'TAG_STORED\r\n'
+
+    resp = server_request(server, 'tag_delete testtag\r\n')
+    assert resp == 'TAG_DELETED\r\n', resp
+
+    resp = server_request(server, 'get testkey\r\n')
+    assert resp == 'END\r\n', resp
+
+
+def test_deleting_tag_with_multiple_keys():
+    data = {}
+    store = taggingcache.TaggingStore(data, writeable=True)
+    server = taggingcache.taggingserver(store)
+    resp = server_request(server, 'set testkey1 0 0 4\r\n'
+                                  'blah\r\n')
+    assert resp == 'STORED\r\n'
+
+    resp = server_request(server, 'set testkey2 0 0 4\r\n'
+                                  'blah\r\n')
+    assert resp == 'STORED\r\n'
+
+    resp = server_request(server, 'tag_add testtag testkey1\r\n')
+    assert resp == 'TAG_STORED\r\n'
+
+    resp = server_request(server, 'tag_add testtag testkey2\r\n')
+    assert resp == 'TAG_STORED\r\n'
+
+    resp = server_request(server, 'tag_delete testtag\r\n')
+    assert resp == 'TAG_DELETED\r\n', resp
+
+    resp = server_request(server, 'get testkey1\r\n')
+    assert resp == 'END\r\n', resp
+
+    resp = server_request(server, 'get testkey2\r\n')
+    assert resp == 'END\r\n', resp
+
+
+def test_deleting_nonexistent_tag():
+    data = {}
+    store = taggingcache.TaggingStore(data, writeable=True)
+    server = taggingcache.taggingserver(store)
+
+    resp = server_request(server, 'tag_delete testtag\r\n')
+    assert resp == 'TAG_NOT_FOUND\r\n', resp
+
+
+def test_adding_tag_to_nonexistant_key():
+    data = {}
+    store = taggingcache.TaggingStore(data, writeable=True)
+    server = taggingcache.taggingserver(store)
+
+    resp = server_request(server, 'tag_add testtag testkey\r\n')
+    assert resp == 'TAG_NOT_FOUND\r\n', resp
+
